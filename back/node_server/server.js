7a10fd382d63eaf2d1c2c780c5b4d59c9140896b
JeongSuHwan <akdtlfdl1014@naver.com>
2024-06-15 15:56:04 +0900

    position 띄어쓰기 제거 / 결과 json 파일 이름 명 일시분초로 저장

--- back/node_server/server.js
+++ back/node_server/server.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
-const upload = multer({ dest: 'uploads/' });
+const path = require('path');
 
 const app = express();
 
+// 현재 일시분초를 기반으로 파일 이름 생성
+const generateFileName = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  return `result_${year}${month}${day}_${hours}${minutes}${seconds}.json`;
+};
+
+// 저장소 설정: 파일 이름을 지정
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    const newFileName = generateFileName(); // 저장할 파일 이름 지정
+    cb(null, newFileName);
+  }
+});
+
+// Multer 설정 (파일 업로드를 위한 미들웨어)
+const upload = multer({ storage: storage });
+
 app.post('/receive_result', upload.single('file'), (req, res) => {
   console.log('Received file:', req.file);
-  
-  fs.readFile(req.file.path, 'utf8', (err, data) => {
+
+  const filePath = path.join(__dirname, 'uploads', req.file.filename);
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) return res.status(500).send('Error reading file');
     const jsonData = JSON.parse(data);
     console.log(JSON.stringify(jsonData, null, 4)); // JSON 데이터 사용 가능
